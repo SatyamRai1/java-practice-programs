ffb5c51b43e50747efd0889e6b88c30925a6d6e7
Satyam Rai <satyamr05@gmail.com>
2021-01-02 11:18:06 +0530

    Add files via upload

---
+++ DayOfTheWeek.java
@@ -0,0 +1,33 @@
+public class DayOfTheWeek {
+    public static void main(String[] args) {
+        printDayOfTheWeek(4);
+    }
+    public static void printDayOfTheWeek(int day){
+        switch(day){
+            case 0:
+                System.out.println("sunday");
+                break;
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+    }
+}
