0e5406836d4e5732713c19c37bfc29d80c4f23fc
Satyam Rai <satyamr05@gmail.com>
2021-01-05 01:17:51 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,26 @@
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(5));
+    }
+
+
+    public static boolean isPerfectNumber(int number){
+        if(number<1{
+            return false;
+        }else{
+            int sum=0;
+            for(int i=1;i<number;i++){
+                if(number%i==0){
+                    sum+=i;
+                }
+            }
+            if (number==sum){
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+
+}
