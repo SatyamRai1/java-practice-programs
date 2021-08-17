3b00c2e615a305b5f97fad277c9d85e4c8284865
Satyam Rai <satyamr05@gmail.com>
2021-08-18 02:12:28 +0530

    Add files via upload

---
+++ AllBasicPrograms/Mn.java
@@ -0,0 +1,28 @@
+public class Mn {
+    public static void main(String[] args) {
+        // System.out.println(AllBasic.isprime(7));
+        //System.out.println(AllBasic.printFactorialRec(8));
+        // System.out.println(AllBasic.printFactLoop(5));
+        //AllBasic.printFibonacci(50);
+        //System.out.println(AllBasic.findGCD(8,12));
+        //System.out.println(AllBasic.findLCM(5,7));
+        //System.out.println(AllBasic.reverseANumber(123));
+        //AllBasic.swap(5,6);
+        //System.out.println(AllBasic.Armstrong(408));
+        //System.out.println(AllBasic.isPalindromeNum(153515351));
+        //System.out.println("reverse: "+AllBasic.reverseDaString("Hello"));
+        /*_________________________________BINARY SEARCH____________________________________
+        int arr[] = {10,20,30,40,50};
+        int key = 30;
+        int last=arr.length-1;
+        AllBasic.binarySearch(arr,0,last,key);
+         */
+
+        /*_________________________________BUBBLE SORT_______________________________________
+        int arr1[]={3,2,7,9,55,5,1,4};
+        AllBasic.bubbleSort(arr1);
+         */
+
+    }
+
+}
